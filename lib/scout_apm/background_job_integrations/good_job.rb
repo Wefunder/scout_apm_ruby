47e5eaf2cbb40aa177fe4baa11d972042bb5d2e9
Ben Garcia <beng4606@gmail.com>
2024-08-06 19:44:59 -0400

    Respect Differences in ActiveJob/GoodJob API

--- lib/scout_apm/background_job_integrations/good_job.rb
+++ lib/scout_apm/background_job_integrations/good_job.rb
@@ -1,6 +1,7 @@
 module ScoutApm
   module BackgroundJobIntegrations
     class GoodJob
+      UNKNOWN_QUEUE_PLACEHOLDER = 'default'.freeze
       attr_reader :logger
 
       def initialize(logger)
@@ -29,13 +30,13 @@ module ScoutApm
             # Maybe hook into GoodJob lifecycle events?
             ScoutApm::Agent.instance.start_background_worker unless ScoutApm::Agent.instance.background_worker_running?
             req = ScoutApm::RequestManager.lookup
-            latency = Time.now - job.scheduled_at rescue 0
+            latency = Time.now - (job.scheduled_at || job.enqueued_at) rescue 0
             req.annotate_request(queue_latency: latency)
 
             begin
-              req.start_layer ScoutApm::Layer.new("Queue", job.queue_name)
+              req.start_layer ScoutApm::Layer.new("Queue", job.queue_name.presence || UNKNOWN_QUEUE_PLACEHOLDER)
               started_queue = true # Following Convention
-              req.start_layer ScoutApm::Layer.new("Job", job.job_class)
+              req.start_layer ScoutApm::Layer.new("Job", job.class.name)
               started_job = true # Following Convention
 
               block.call
