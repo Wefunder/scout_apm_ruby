0b6bac2ddc1c6cb49219fbf00ce77549e6667cc1
Ben Garcia <beng4606@gmail.com>
2024-08-07 00:26:42 -0400

    Remove erroneous debugger

--- lib/scout_apm/environment.rb
+++ lib/scout_apm/environment.rb
@@ -148,7 +148,6 @@ module ScoutApm
 
     def background_job_integrations
       if Agent.instance.context.config.value("enable_background_jobs")
-        binding.b
         @background_job_integrations ||= BACKGROUND_JOB_INTEGRATIONS.select {|integration| integration.present?}
       else
         []
